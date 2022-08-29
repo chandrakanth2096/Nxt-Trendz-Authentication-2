39f2d1d4e7be5be5cc7f645b2539b398ce36f32a
chandrakanth2096 <chandrakanth2096@gmail.com>
2022-08-29 13:07:27 +0000

    nxt auth 2

---
+++ src/components/Products/index.js
@@ -0,0 +1,27 @@
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+import Header from '../Header'
+import './index.css'
+
+const Products = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="product-section">
+        <img
+          className="product"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Products
